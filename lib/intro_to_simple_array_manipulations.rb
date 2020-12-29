9294efcf4a7b739cc40eee3adfb1e19a825dd855
calm-collection-3916 <khalinredd@gmail.com>
2020-12-29 15:22:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+
+def using_push(array, element)
+  push_array = ["hello", "greetings", "bye"]
+  push_array.push("violet")
+end
+
+def using_unshift(array, string)
+  unshift_array = Array.new(4)
+  unshift_array.unshift("Staten Island")
+end
+ 
+def using_pop(array)
+  pop_array = ["hello", "he", "did", "Antarctica"]
+  pop_array.pop
+end
+
+def pop_with_args(array)
+  pop_array_args = ["hi", "Chihuahua", "Shiba Inu"]
+  pop_array_args.pop(2)
+end
+
+def using_shift(array)
